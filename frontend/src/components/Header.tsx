22a8c0eece3b00c3455519cc6a1c75d4a880b9b9
Philipp Schoenborn <philipp.schoenborn@example.com>
2025-07-07 16:23:39 +0200

    add responsive

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,6 +5,7 @@ import logoSvg from '../assets/Logo.svg';
 import styles from './Header.module.css';
 import Button from './Button';
 import LogoutButton from './LogoutButton';
+import BurgerMenu from './BurgerMenu';
 
 const Header: React.FC = () => {
   const location = useLocation();
@@ -18,13 +19,18 @@ const Header: React.FC = () => {
     <header className={styles.header}>
       <img src={logoSvg} alt="Videoflix Logo" />
       {!hideCTAButton && (
-        <div className={styles.rightSection}>
+        <div className={styles.headerRight}>
           {isAuthenticated ? (
             <LogoutButton />
           ) : (
-            <Link to="/login" className={styles.loginLink}>
-              <Button>Login</Button>
-            </Link>
+            <>
+              <div className={styles.rightSection + ' ' + styles.hideOnMobile}>
+                <Link to="/login" className={styles.loginLink}>
+                  <Button>Login</Button>
+                </Link>
+              </div>
+              <BurgerMenu />
+            </>
           )}
         </div>
       )}
